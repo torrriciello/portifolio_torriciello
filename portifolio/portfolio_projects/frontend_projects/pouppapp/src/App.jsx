675be00b934f5c34c9f1e2cd0944bf225849bac8
Gabriel Torriciello <gabrielvictorsantostorriciello@gmail.com>
2026-06-18 15:57:15 -0300

    creating poupApp project

---
+++ portifolio/portfolio_projects/frontend_projects/pouppapp/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './assets/logo.svg'
+import { Container } from './components/Container'
+
+function App() {
+  return (
+    <Container>
+      <aside>
+        <img src={logo} alt="" />
+      </aside>
+    </Container>
+  )
+}
+
+export default App
