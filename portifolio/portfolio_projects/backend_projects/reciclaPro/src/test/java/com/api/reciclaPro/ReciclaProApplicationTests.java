675658d10872ada7ad872c7ca3d76fc740f10df8
Gabriel_Torriciello <gabrielvictorsantostorriciello@gmail.com>
2026-06-28 13:02:17 -0300

    commit

---
+++ portifolio/portfolio_projects/backend_projects/reciclaPro/src/test/java/com/api/reciclaPro/ReciclaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.reciclaPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
